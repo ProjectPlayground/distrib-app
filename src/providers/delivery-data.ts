29dcff1beca1c5b014104c840c994ec08650da7d
Tony <t.qian7@gmail.com>
2016-10-12 05:36:50 -0600

    index.html and delivery data provider

--- src/providers/delivery-data.ts
+++ src/providers/delivery-data.ts
@@ -1,18 +1,64 @@
+import { Events } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { AuthHttp } from 'angular2-jwt';
+import { AuthService } from '../providers/auth';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the DeliveryData provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class DeliveryData {
 
-  constructor(public http: Http) {
-    console.log('Hello DeliveryData Provider');
+  apiUrl = 'https://server-distrib.rhcloud.com/api/';
+
+  constructor (
+  	public http: Http, 
+  	public authHttp: AuthHttp, 
+  	public auth: AuthService, 
+  	public events: Events, 
+  	public storage: Storage
+ 	) {}
+
+  getShifts(filters?) {
+    let query = this.apiUrl+'shifts?driver='+this.auth.user._id;
+    if (filters) {
+      if (filters.start) {
+        query = query+'&start=' + filters.start;
+      }
+      if (filters.end) {
+        query = query+'&end=' + filters.end;
+      }
+      if (filters.id) {
+        query = this.apiUrl+'shifts/'+filters.id;
+      }
+    }
+    return this.authHttp.get(query)
+      .map(res => res.json());
+  }
+
+  postShift(shift) {
+    return this.authHttp.post(this.apiUrl+'shifts', shift)
+      .map(res => res.json());
+  }
+
+  deleteShift(shift) {
+    return this.authHttp.delete(this.apiUrl+'shifts/'+shift._id)
+  }
+
+  waypointStatus(shift, waypoint, status) {
+    return this.authHttp.patch(this.apiUrl+'shifts/'+shift._id+'/waypoints/'+waypoint._id, {status: status})
+      .map(res => res.json());
+  }
+
+  setCurrentShift(shift) {
+    this.storage.set('currentShift', shift._id); // save id of current shift (cant save objects)
+  }
+
+  getCurrentShift() {
+    return this.storage.get('currentShift').then(value => {
+      return value;
+    });
   }
 
 }
